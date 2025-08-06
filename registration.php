aad14ac83b10b54a018c2ae8b4ed2f496ab171b8
BallesteroDavid <david.ballestero@outlook.fr>
2025-08-06 11:30:57 +0200

    first commit

---
+++ registration.php
@@ -0,0 +1,50 @@
+<?php
+
+    // --------------------------------------------------
+    // Condition qui contient la logique de traitement du formulaire quand on reçoit une request POST
+    // --------------------------------------------------
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Récupation des données du formulaire
+    $username = $_POST["username"] ??'';;
+    $email = $_POST["email"] ??'';;
+    $password = $_POST["password"] ??'';;
+    $confirmPassword = $_POST["confirmPassword"] ??'';;
+    var_dump($_POST);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="sectionContainer">
+        <form action="" method="POST">
+            <div class="sectionContainerUsername">
+                <label for="Pseudo">Pseudo :</label><br>
+                <input placeholder="Entrez votre pseudo" type="text" id="username" name="username" required><br>
+            </div>
+            <div class="sectionContainerEmail">
+                <label for="Email">Email :</label><br>
+                <input placeholder="Entrez votre email" type="email" id="email" name="email" required><br>
+            </div>
+            <div class="sectionContainerPassword">
+                <label for="password">Password :</label><br>
+                <input placeholder="Entrez votre password" type="password" id="password" name="password" required><br>
+            </div>
+            <div class="sectionContainerConfirmPassword">
+                <label for="confirmPassword"> Confirmer votre password :</label><br>
+                <input placeholder="Confirm Password" type="password" id="confirmPassword" required><br>
+            </div>
+            <div class="sectionContainerBtn">
+                <input type="submit" value="Envoyer"><br>
+            </div>
+        </form>
+    </section>
+    
+</body>
+</html>
